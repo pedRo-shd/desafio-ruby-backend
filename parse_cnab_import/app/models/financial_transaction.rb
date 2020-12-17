64edf1a49aee932e17b4f539647251fddb3246e3
Pedro <pedro.c.shd@gmail.com>
2020-12-17 01:57:58 -0300

    Record amount positive and negative depending on the transaction kind

--- parse_cnab_import/app/models/financial_transaction.rb
+++ parse_cnab_import/app/models/financial_transaction.rb
@@ -1,3 +1,24 @@
 class FinancialTransaction < ApplicationRecord
   belongs_to :store
+
+  enum transaction_kind: %i[
+    t_empty
+    debit
+    bank_slip
+    financing
+    credit
+    loan_receipt
+    sales
+    ted_receipt
+    doc_receipt
+    rent
+  ]
+
+  before_save :formated_amount_transaction_operation
+
+  private
+
+  def formated_amount_transaction_operation
+    self.amount = -self.amount if ["bank_slip", "financing", "rent"].any? { |i| [transaction_kind].include? i }
+  end
 end
