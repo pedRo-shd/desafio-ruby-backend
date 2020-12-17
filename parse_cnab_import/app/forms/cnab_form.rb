e2dd69c08861cf4a10f8c9c966f94e5cef8aa161
Pedro <pedro.c.shd@gmail.com>
2020-12-17 02:00:13 -0300

    Change OpenStruct to CnabForm attributes

---
+++ parse_cnab_import/app/forms/cnab_form.rb
@@ -0,0 +1,9 @@
+class CnabForm < Form
+  attribute :transaction_kind
+  attribute :occurrence_at
+  attribute :amount
+  attribute :document_value
+  attribute :card_number
+  attribute :owner
+  attribute :brand
+end
